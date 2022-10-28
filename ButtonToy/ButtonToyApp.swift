5bee4603d63df00da986b42902f6718f6a2916f7
kxx <132kxx@gmail.com>
2022-10-28 13:47:16 +0900

    Initial Commit

---
+++ ButtonToy/ButtonToyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonToyApp.swift
+//  ButtonToy
+//
+//  Created by kxx: on 2022/10/28.
+//
+
+import SwiftUI
+
+@main
+struct ButtonToyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
